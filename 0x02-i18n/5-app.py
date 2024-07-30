7a399d820fb329a06b41623cf09a0551957bb035
ngwenjeursular <ngwenjeursular@gmail.com>
2024-07-30 21:22:39 +0000

    update some files

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,25 +1,10 @@
 #!/usr/bin/env python3
 """Module for task 5
 """
-from typing import Dict, Union
 from flask import Flask, render_template, request, g
-from flask_babel import Babel
+from flask_babel import Babel, _
 
-app = Flask(__name__)
-
-app.url_map.strict_slashes = False
-
-
-class Config:
-    """Represents a Flask Babel configuration.
-    """
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
-
-
-app.config.from_object(Config)
-babel = Babel(app)
+# Mock user database
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
     2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
@@ -28,70 +13,69 @@ users = {
 }
 
 
-@app.route("/")
-def index_5() -> str:
-    """The index function displays the home page of the web application.
-
-    Returns:
-        str: contents of the home page.
+class Config:
     """
-    return render_template("5-index.html")
+    Configuration class for the Flask app.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel(app)
 
 
 @babel.localeselector
-def get_locale() -> str:
-    """Determines the best match for the client's preferred language.
-
-    This function uses Flask's request object to access the client's preferred
-    languages and the app's supported languages (defined in the Config class)
-    to determine the best match. The best match is then returned as the locale.
-
-    Returns:
-        str: The locale code for the best match (e.g. "en", "fr").
+def get_locale():
     """
-    # Get the locale parameter from the incoming request
+    Determine the best match for supported languages
+    or use the locale from the URL.
+
+    :return: Best match locale or the one provided in the URL.
+    """
     locale = request.args.get('locale')
-    # Get list of supported languages from Config
-    supported_languages = app.config["LANGUAGES"]
-    if locale and locale in supported_languages:
-        # If the locale parameter is present and is a supported locale,
-        # return it
+    if locale in app.config['LANGUAGES']:
         return locale
-    else:
-        # Use request.accept_languages to get the best match
-        best_match = request.accept_languages.best_match(supported_languages)
-        return best_match
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-def get_user() -> Union[Dict, None]:
-    """Returns a user dictionary based on the given ID
-
-    Returns:
-        Union[Dict, None]: The user dictionary if found, otherwise None.
+def get_user():
     """
-    # Get the user_id from the login_as URL parameters
-    login_id = request.args.get('login_as')
-    # If the user ID exists in the URL parameters
-    if login_id:
-        # Get the user dictionary from the `users` dictionary using the user ID
-        return users.get(int(login_id))
-    # If the user ID does not exist in the URL parameters, return None
+    Mock function to get user information based on the login_as parameter
+
+    :return: User dictionary or None if not found or not logged in.
+    """
+    user_id = request.args.get('login_as')
+    if user_id and user_id.isdigit():
+        user_id = int(user_id)
+        return users.get(user_id)
     return None
 
 
 @app.before_request
-def before_request() -> None:
-    """Function to be executed before every request.
+def before_request():
     """
-    # Use the get_user function to get the user details
-    user = get_user()
-    # Set the user as a global variable on flask.g
-    g.user = user
+    Set the global user based on the login_as parameter if provided.
+    """
+    g.user = get_user()
 
 
-# Register the before_request function to be executed before every request
-app.before_request(before_request)
+@app.route('/')
+def index():
+    """
+    Render the index.html template with localized
+    messages and user-specific information.
+
+    :return: Rendered HTML for the index page.
+    """
+    return render_template('5-index.html')
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    """
+    Run the Flask application.
+    """
+    app.run(host='0.0.0.0', port=5000)
