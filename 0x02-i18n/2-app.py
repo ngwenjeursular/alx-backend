bd6e9fce1c17ac457cf69220c2383748857834f8
ngwenjeursular <ngwenjeursular@gmail.com>
2024-07-30 20:14:55 +0000

    Get locale from request

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
+"""Module for task 2
 """
-module for task 1
-"""
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
@@ -21,14 +20,24 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
+@babel.localeselector
+def get_locale():
+    """
+    Determine the best match for supported languages.
+
+    :return: Best match locale.
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
 @app.route('/')
 def index():
     """
     Render the index.html template.
-    
+
     :return: Rendered HTML for the index page.
     """
-    return render_template('1-index.html')
+    return render_template('2-index.html')
 
 
 if __name__ == '__main__':
